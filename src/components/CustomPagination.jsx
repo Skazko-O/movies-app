576a8413853e5adcd626256c90aa427e28c2ae61
Skazko_o <skazko@outlook.com>
2025-09-16 15:59:06 +0300

    feat: update movie ticket icon, implement pagination in search results, and enhance About page content

--- src/components/CustomPagination.jsx
+++ src/components/CustomPagination.jsx
@@ -1,5 +1,45 @@
 import Pagination from 'react-bootstrap/Pagination';
 
-export default function CustomPagination() {
-    return
+export default function CustomPagination({ currentPage, totalPages, onPageChange }) {
+    const paginationItems = [];
+
+    if (currentPage > 3) {
+        paginationItems.push(<Pagination.Item key={1} onClick={() => onPageChange(1)}>{1}</Pagination.Item>);
+        paginationItems.push(<Pagination.Ellipsis key="start-ellipsis" disabled />);
+    }
+
+    for (let page = currentPage - 2; page <= currentPage + 2; page++) {
+        if (page > 0 && page <= totalPages) {
+           
+            paginationItems.push(
+                <Pagination.Item
+                    key={page}
+                    active={page === currentPage}                    
+                    onClick={() => onPageChange(page)}
+                >
+                    {page}
+                </Pagination.Item>
+            );
+        }
+    }
+
+    if (currentPage < totalPages - 2) {
+        paginationItems.push(<Pagination.Ellipsis key="end-ellipsis" disabled />);
+        paginationItems.push(
+            <Pagination.Item key={totalPages} onClick={() => onPageChange(totalPages)}>
+                {totalPages}
+            </Pagination.Item>
+        );
+    }
+
+    return (
+        <Pagination>
+            <Pagination.First onClick={() => onPageChange(1)} disabled={currentPage === 1} />
+            <Pagination.Prev onClick={() => onPageChange(currentPage - 1)} disabled={currentPage === 1} />
+            {paginationItems}
+        <Pagination.Next onClick={() => onPageChange(currentPage + 1)} disabled={currentPage === totalPages} />
+        <Pagination.Last onClick={() => onPageChange(totalPages)} disabled={currentPage === totalPages} />
+      </Pagination>
+    );
 }
+
--- src/components/SearchResult.jsx
+++ src/components/SearchResult.jsx
@@ -1,8 +1,12 @@
 import { Card, Row, Col } from 'react-bootstrap';
 import MovieCard from './MovieCard';
+import CustomPagination from './CustomPagination';
+import './../assets/css/search.css';
 
-function SearchResult({ movies }) { 
-   const { results, total, totalPages, currentPage } = movies;
+
+function SearchResult({ movies, onPageChange }) {  
+  const { results, total, totalPages, currentPage } = movies;
+  
   return (
     <Card>
       <Card.Header><h4>Search result: {total} | Page {currentPage} of {totalPages}</h4></Card.Header>
@@ -15,6 +19,15 @@ function SearchResult({ movies }) {
           ))}
         </Row>
       </Card.Body>
+      {totalPages > 1 && (
+      <div className='pagination-wrapper'>
+        <CustomPagination
+        currentPage={currentPage}
+        totalPages={totalPages}
+        onPageChange={onPageChange}  
+        />
+      </div>
+      )}
     </Card>
   );
 }
